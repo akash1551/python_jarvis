c49a15ac0457d2b4bb2903e277addcab3d02cacb
akash1551 <acwankhede@gmail.com>
2017-03-21 18:31:22 +0530

    added modified file

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
+
 setup(
   name = 'python_jarvis',
   packages = ['python_jarvis'], # this must be the same as the name above
@@ -9,5 +11,7 @@ setup(
   url = 'https://github.com/akash1551/python_jarvis.git', # use the URL to the github repo
   # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  classifiers = ['Development Status :: 4 - Beta',
+                 'Intended Audience :: System Administrators',
+                 'Programming Language :: Python :: 2.7'],
 )
