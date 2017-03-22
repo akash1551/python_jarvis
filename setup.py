e4b95d7d43cb792e452cac81647dd6674b940d10
akash1551 <acwankhede@gmail.com>
2017-03-22 13:59:03 +0530

    added modified files

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 from setuptools import setup
 
 setup(
-  name = 'python_jarvis',
-  packages = ['python_jarvis'], # this must be the same as the name above
+  name = 'python-jarvis',
+  packages = ['python-jarvis'], # this must be the same as the name above
   version = '0.1',
   description = 'Simple helper functions for python projects',
   author = 'Akash Wankhede',
   author_email = 'acwankhede@gmail.com',
   url = 'https://github.com/akash1551/python_jarvis.git', # use the URL to the github repo
   # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  keywords = ['python helper functions', 'quicktool', 'regular used functions'], # arbitrary keywords
   classifiers = ['Development Status :: 4 - Beta',
                  'Intended Audience :: System Administrators',
                  'Programming Language :: Python :: 2.7'],
