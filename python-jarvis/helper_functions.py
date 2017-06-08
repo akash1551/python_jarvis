af06f6d885c43c5362c6233ae511f7087efd7531
akash1551 <acwankhede@gmail.com>
2017-06-08 13:15:21 +0530

    added modified file

--- python-jarvis/helper_functions.py
+++ python-jarvis/helper_functions.py
@@ -1,7 +1,4 @@
 import datetime
 
 def convert_epoch_to_date(epoch):
-    if not epoch:
-        return None
-
     return datetime.datetime.fromtimestamp(long(epoch)/1000.0) if epoch else None
--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
   author = 'Akash Wankhede',
   author_email = 'acwankhede@gmail.com',
   url = 'https://github.com/akash1551/python_jarvis.git', # use the URL to the github repo
-  # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/akash1551/python_jarvis/archive/master.zip',
   keywords = ['python helper functions', 'quicktool', 'regular used functions'], # arbitrary keywords
   classifiers = ['Development Status :: 4 - Beta',
                  'Intended Audience :: System Administrators',
-                 'Programming Language :: Python :: 2.7'],
+                 'Programming Language :: Python :: 2.7']
 )
